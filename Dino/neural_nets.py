a256229e87afcc0601b9a998c08e1d994d58eb3d
Vitor Bonella <vitorb.bonella36@gmail.com>
2022-07-01 19:26:26 -0300

    file

--- Dino/neural_nets.py
+++ Dino/neural_nets.py
@@ -12,5 +12,6 @@ net = tensorflow.keras.Model(inputs=input_layer,outputs=output_layer)
 net_genetic_alg = pygad.kerasga.KerasGA(model=net,num_solutions = 15)
 
 def on_generation(ga_instance):
-    print("Generation = {generation}".format(generation=ga_instance.generations_completed))
-    print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution(ga_instance.last_generation_fitness)[1]), end='\n\n')
+    file = open("generations.txt",mode='a')
+    file.write("Generation = {generation}\n".format(generation=ga_instance.generations_completed))
+    file.write("Fitness    = {fitness}\n\n".format(fitness=ga_instance.best_solution(ga_instance.last_generation_fitness)[1]))
